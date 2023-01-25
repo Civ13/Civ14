95d0c2d56a10a1378f3382409e09c2b924dcac31
Taislin <cariosecus@gmail.com>
2023-01-25 22:44:28 +0000

    civ sprites and metadata

---
+++ Content.Client/_Frigid/Sun/SunSystem.cs
@@ -0,0 +1,34 @@
+using Content.Shared.Sun;
+using Robust.Client.Graphics;
+
+namespace Content.Client.Sun;
+
+public sealed class SunSystem : SharedSunSystem
+{
+    [Dependency] private readonly LightManager _lightManager = default!;
+
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void OnTimeChangeEvent(TimeChangeEvent message, EntitySessionEventArgs eventArgs)
+    {
+        switch (message.TimeOfDay)
+        {
+            case Time.Midnight:
+                _lightManager.AmbientLightColor = MidnightColor;
+                break;
+            case Time.Day:
+                _lightManager.AmbientLightColor = DayColor;
+                break;
+            case Time.Noon:
+                _lightManager.AmbientLightColor = NoonColor;
+                break;
+            case Time.Night:
+                _lightManager.AmbientLightColor = NightColor;
+                break;
+        }
+    }
+}
--- testrun.bat
+++ testrun.bat
@@ -1,3 +1,2 @@
 start dotnet run --project Content.Server --configuration Release
 start dotnet run --project Content.Client --configuration Release
-pause
