c12189ee75ddf2aa2b3b574c3318df6b626d898b
Bierkraan <jarnohofstede@outlook.com>
2023-02-09 12:26:37 +0100

    I have no clue what I'm doing AAAAAA

--- Content.Server/Tree/TreeComponent.cs
+++ Content.Server/Tree/TreeComponent.cs
@@ -5,4 +5,7 @@ public sealed class TreeComponent : Component
 {
     [DataField("sound")]
     public string Sound = string.Empty;
+
+    [DataField("breakTime")]
+    public float BreakTime = 3.0f;
 }
--- Content.Server/Tree/TreeSystem.cs
+++ Content.Server/Tree/TreeSystem.cs
@@ -1,4 +1,5 @@
 using Content.Server.Tree;
+using Content.Server.DoAfter;
 using Content.Shared.Interaction;
 
 namespace Content.Server.Tree;
@@ -6,11 +7,12 @@ namespace Content.Server.Tree;
 public sealed class TreeSystem : EntitySystem
 {
     [Dependency] private readonly SharedInteractionSystem _interactionSystem = default!;
-
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly DoAfterSystem _doAfterSystem = default!;
     public override void Initialize()
     {
-        SubscribeLocalEvent<TreeComponent, InteractHandEvent>(OnInteractHand);
+        SubscribeLocalEvent<TreeComponent, AfterInteractEvent>(AfterInteractOn);
+        SubscribeLocalEvent<TreeComponent>(OnDoAfterComplete);
     }
 
     private void OnInteractHand(EntityUid uid, TreeComponent component, InteractHandEvent args)
@@ -20,6 +22,46 @@ public sealed class TreeSystem : EntitySystem
         var pos = Transform(uid).MapPosition;
         EntityManager.SpawnEntity("SharpenedStick", pos);
         _audio.PlayPvs(component.Sound, uid);
+    }
+
+    private void OnDoAfterComplete(EntityUid uid, TreeComponent component)
+        {
+            var pos = Transform(uid).MapPosition;
+            EntityManager.SpawnEntity("SharpenedStick", pos);
+            _audio.PlayPvs(component.Sound, uid);
+            }
+        }
+
+    private void AfterInteractOn(EntityUid uid, TreeComponent component, AfterInteractEvent args)
+    {
+        var doAfterEventArgs = new DoAfterEventArgs(args.User, component.BreakTime, default, target)
+        {
+            BreakOnTargetMove = true,
+            BreakOnUserMove = true,
+            BreakOnDamage = true,
+            BreakOnStun = true,
+            NeedHand = true,
+            BroadcastFinishedEvent = new TreeBranchDoAfterComplete(uid, target, component, args.User),
+        };
+        _doAfterSystem.DoAfter(doAfterEventArgs);
+    }
+
+
+    private sealed class TreeBranchDoAfterComplete : EntityEventArgs
+    {
+        public readonly EntityUid User;
+        public readonly EntityUid UsedTool;
+        public readonly EntityUid Target;
+        public readonly TreeComponent Component;
+
+        public TreeBranchDoAfterComplete(EntityUid usedTool, EntityUid target, string sprite,
+            TreeComponent component, EntityUid user)
+        {
+            User = user;
+            UsedTool = usedTool;
+            Target = target;
+            Component = component;
+        }
     }
 }
 
