3fd46b85329d27f495d3086159a1fd0c8b251994
Alex Sampaio <alexsampaio.contato@gmail.com>
2025-04-03 13:21:13 -0300

    chore: remove dev logs, ballance burn temps

--- Content.Server/Civ14/Kitchen/GrillFuelBurnSystem.cs
+++ Content.Server/Civ14/Kitchen/GrillFuelBurnSystem.cs
@@ -39,7 +39,6 @@ public sealed class GrillFuelBurnSystem : EntitySystem
 
     private void OnMapInit(EntityUid uid, GrillFuelBurnComponent component, MapInitEvent args)
     {
-        Log.Debug($"ON MAP INIT for campfire {uid}");
         _remainingBurnTime[uid] = component.Fuel * 2f * 60f;
         component.IsLit = false;
     }
@@ -47,7 +46,6 @@ public sealed class GrillFuelBurnSystem : EntitySystem
     private void OnItemPlaced(EntityUid uid, GrillFuelBurnComponent comp, ref ItemPlacedEvent args)
     {
         var itemProto = _entityManager.GetComponent<MetaDataComponent>(args.OtherEntity).EntityPrototype?.ID;
-        Log.Debug($"Item placed on campfire: {itemProto}");
     }
 
     private void OnInteractUsing(EntityUid uid, GrillFuelBurnComponent comp, InteractUsingEvent args)
@@ -58,21 +56,18 @@ public sealed class GrillFuelBurnSystem : EntitySystem
             {
                 comp.IsLit = true;
                 AdjustHeaterSetting(uid, comp);
-                Log.Debug($"Campfire {uid} lit by {args.Used}");
             }
             args.Handled = true;
             return;
         }
 
-        // Lógica existente para adicionar combustível
+        // Add fuel
         if (!_entityManager.TryGetComponent<BurnFuelComponent>(args.Used, out var burnFuel))
         {
-            Log.Debug($"Item {args.Used} has no BurnFuelComponent");
             return;
         }
 
         var itemProto = _entityManager.GetComponent<MetaDataComponent>(args.Used).EntityPrototype?.ID;
-        Log.Debug($"InteractUsing on campfire with: {itemProto}, BurnTime: {burnFuel.BurnTime}");
 
         if (_entityManager.TryGetComponent<StackComponent>(args.Used, out var stackComp))
         {
@@ -82,39 +77,28 @@ public sealed class GrillFuelBurnSystem : EntitySystem
 
             if (fuelToAdd > 0)
             {
-                Log.Debug($"Adding {fuelToAdd} fuel units from stack of {stackComp.Count}");
                 comp.Fuel += fuelToAdd;
                 _remainingBurnTime[uid] += fuelToAdd * burnFuel.BurnTime * 60f;
                 _stackSystem.SetCount(args.Used, stackComp.Count - fuelToAdd, stackComp);
 
                 if (stackComp.Count <= 0)
                 {
-                    Log.Debug("Stack depleted, deleting item");
                     QueueDel(args.Used);
                 }
 
                 AdjustHeaterSetting(uid, comp);
                 args.Handled = true;
             }
-            else
-            {
-                Log.Debug("Fuel at max, no fuel added from stack");
-            }
         }
         else
         {
             if (comp.Fuel < comp.MaxFuel)
             {
-                Log.Debug("Adding 1 fuel unit from non-stack item");
                 comp.Fuel++;
                 _remainingBurnTime[uid] += burnFuel.BurnTime * 60f;
                 QueueDel(args.Used);
                 AdjustHeaterSetting(uid, comp);
                 args.Handled = true;
-            }
-            else
-            {
-                Log.Debug("Fuel at max, item not consumed");
             }
         }
     }
@@ -167,7 +151,6 @@ public sealed class GrillFuelBurnSystem : EntitySystem
             return;
 
         comp.Setting = setting;
-        Log.Debug($"Changing setting to {setting} for campfire {uid}");
         _appearance.SetData(uid, EntityHeaterVisuals.Setting, setting);
 
         // Adjust the PointLight based on the Setting
@@ -202,7 +185,7 @@ public sealed class GrillFuelBurnSystem : EntitySystem
     }
     private void AdjustHeaterSetting(EntityUid uid, GrillFuelBurnComponent comp)
     {
-        if (!comp.IsLit) // Se não estiver acesa, força o estado Off
+        if (!comp.IsLit) // If its not lit, updates to Off
         {
             if (comp.Setting != EntityHeaterSetting.Off)
             {
@@ -214,18 +197,17 @@ public sealed class GrillFuelBurnSystem : EntitySystem
         var remainingTimeSeconds = _remainingBurnTime.GetValueOrDefault(uid);
         EntityHeaterSetting newSetting;
 
-        if (remainingTimeSeconds > 600f) // > 10 minutos
+        if (remainingTimeSeconds > 600f) // > 10 minutes
             newSetting = EntityHeaterSetting.High;
-        else if (remainingTimeSeconds > 300f) // 5-10 minutos
+        else if (remainingTimeSeconds > 300f) // 5-10 minutes
             newSetting = EntityHeaterSetting.Medium;
-        else if (remainingTimeSeconds > 0f) // < 5 minutos
+        else if (remainingTimeSeconds > 0f) // < 5 minutes
             newSetting = EntityHeaterSetting.Low;
         else
             newSetting = EntityHeaterSetting.Off;
 
         if (comp.Setting != newSetting)
         {
-            Log.Debug($"Adjusting heater setting from {comp.Setting} to {newSetting} (Remaining Time: {remainingTimeSeconds / 60f:F1} minutes)");
             ChangeSetting(uid, newSetting, comp);
         }
     }
@@ -235,11 +217,11 @@ public sealed class GrillFuelBurnSystem : EntitySystem
         switch (setting)
         {
             case EntityHeaterSetting.Low:
-                return 800f;
+                return 400f;
             case EntityHeaterSetting.Medium:
-                return 1600f;
+                return 800f;
             case EntityHeaterSetting.High:
-                return 2400f;
+                return 1600f;
             default:
                 return 0f;
         }
